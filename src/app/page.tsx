e7adb5c5d736f1f6624efee67d389549ce1f4905
marcin0s <mkowalczyk193@gmail.com>
2024-04-08 20:42:35 +0200

    add ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,26 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import AuctionCard from "@/components/AuctionCard";
 import { Auction } from "./types/Auction";
 import FilterSheet from "@/components/FilterSheet";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-    const searchParams = useSearchParams();
+export default async function Home({ searchParams }: { searchParams: any }) {
+    const { brand, productionFrom, productionTo, mileageFrom, mileageTo, auctionEndBefore } = searchParams;
+    const auctions = await getAuctions(
+        brand ? brand : null,
+        productionFrom ? new Date(productionFrom) : null,
+        productionTo ? new Date(productionTo) : null,
+        mileageFrom ? parseInt(mileageFrom) : null,
+        mileageTo ? parseInt(mileageTo) : null,
+        auctionEndBefore ? new Date(auctionEndBefore) : null
+    );
 
-    const brand = searchParams.get("brand");
-    const productionFrom = searchParams.get("productionFrom");
-    const productionTo = searchParams.get("productionTo");
-    const mileageFrom = searchParams.get("mileageFrom");
-    const mileageTo = searchParams.get("mileageTo");
-    const auctionEndBefore = searchParams.get("auctionEndBefore");
-
-    const [auctions, setAuctions] = useState<Auction[] | null>(null);
-
-    useEffect(() => {
-        (async () => {
-            const auctionsResponse = await getAuctions(
-                brand ? brand : null,
-                productionFrom ? new Date(productionFrom) : null,
-                productionTo ? new Date(productionTo) : null,
-                mileageFrom ? parseInt(mileageFrom) : null,
-                mileageTo ? parseInt(mileageTo) : null,
-                auctionEndBefore ? new Date(auctionEndBefore) : null
-            );
-            if (auctionsResponse instanceof Error) return;
-            setAuctions(auctionsResponse);
-        })();
-    }, []);
-
-    if (auctions === null) {
+    if (auctions instanceof Error) {
         return (
             <div className="mt-6 max-w-7xl container px-2 sm:px-3 mx-auto">
                 <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center gap-3">
-                    <div className="w-full flex flex-col">
-                        <h1 className="text-3xl font-bold">Dostępne aukcje</h1>
-                        <p className="mt-2 text-muted-foreground">
-                            Sprawdź listę wszystkich aukcji samochodowych ze strony auta.ch
-                        </p>
-                    </div>
-                    <FilterSheet />
-                </div>
-                <div className="my-6 flex justify-center items-center">
-                    <div role="status">
-                        <svg
-                            aria-hidden="true"
-                            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                            />
-                            <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                            />
-                        </svg>
+                    <div className="my-6">
+                        <span className="text-xl underline">
+                            Wystąpił błąd podczas przetwarzania zapytania ze strony auta.ch. Spróbuj ponownie później.
+                        </span>
                     </div>
                 </div>
             </div>
@@ -102,14 +61,10 @@ async function getAuctions(
     auctionEndBefore: Date | null
 ): Promise<Auction[] | Error> {
     try {
-        const requestOptions = {
-            method: "GET",
-            redirect: "follow",
-            mode: "no-cors"
-          };
-          
-          const response = await fetch("https://auta.ch/api/v1/auctions?type=json", requestOptions)
-            .then((response) => response.json())
+        const response = await fetch(`https://auta.ch/api/v1/auctions/?format=json`, {
+            cache: "no-store",
+            next: { revalidate: 1 }
+        }).then((res) => res.json());
 
         const filteredAuctions = response
             .filter((entry: any) => {
